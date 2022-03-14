f2e5cf46fc1fd0799ff8f346f7022fc0a79440f6
Radoslav Velichkov <rado88x@gmail.com>
2022-03-14 16:47:29 +0200

    added some styling and remove btn

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,21 @@ const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-   setTodos(prevTodos =>[...prevTodos,{id: Math.random().toString(), text: text}])
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
+
+  const todoDeleteHandler = (id : string) =>{
+    setTodos ( prev => { 
+      return prev.filter( todo => todo.id !== id)
+  })}
 
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 };
